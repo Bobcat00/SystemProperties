e485ce031b1396fc832864a245311e88c2e49460
Bobcat00 <Bobcat00@users.noreply.github.com>
2022-10-31 21:38:28 -0400

    Log processor, motherboard, disk, and network info from OSHI Use the Spigot library loader Use Java 17 and MC 1.19 Update to version 2.00

--- src/main/java/com/bobcat00/systemproperties/SystemProperties.java
+++ src/main/java/com/bobcat00/systemproperties/SystemProperties.java
@@ -20,6 +20,7 @@ import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryPoolMXBean;
 import java.lang.management.RuntimeMXBean;
+import java.text.NumberFormat;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -29,11 +30,22 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import com.sun.management.OperatingSystemMXBean;
 
+import oshi.SystemInfo;
+import oshi.hardware.Baseboard;
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.ComputerSystem;
+import oshi.hardware.GlobalMemory;
+import oshi.hardware.HWDiskStore;
+import oshi.hardware.HardwareAbstractionLayer;
+import oshi.hardware.NetworkIF;
+
 public final class SystemProperties extends JavaPlugin {
     
     @Override
     public void onEnable()
     {
+        NumberFormat nf = NumberFormat.getInstance();
+        
         getLogger().info("------------------------------------------------------------");
         Properties pro = System.getProperties();
         Map<Object,Object> sortedMap = new TreeMap<>(pro);
@@ -42,12 +54,74 @@ public final class SystemProperties extends JavaPlugin {
             getLogger().info("System." + (String)obj + ": " + System.getProperty((String)obj));
         }
         
+        // OSHI
+        
+        try
+        {
+            SystemInfo systemInfo = new SystemInfo();
+            HardwareAbstractionLayer hardware = systemInfo.getHardware();
+            // CPU
+            CentralProcessor cpu = hardware.getProcessor();
+            getLogger().info("ProcessorIdentifier.name: " + cpu.getProcessorIdentifier().getName());
+            getLogger().info("CentralProcessor.PhysicalPackages: " + cpu.getPhysicalPackageCount());
+            getLogger().info("CentralProcessor.PhysicalProcessors: " + cpu.getPhysicalProcessorCount());
+            getLogger().info("CentralProcessor.LogicalProcessors: " + cpu.getLogicalProcessorCount());
+            // Memory
+            GlobalMemory memory = hardware.getMemory();
+            getLogger().info("GlobalMemory.total: " + nf.format(memory.getTotal()));
+            // Computer system
+            ComputerSystem computer = hardware.getComputerSystem();
+            getLogger().info("ComputerSystem.manufacturer: " + computer.getManufacturer());
+            if (!computer.getModel().equals("unknown"))
+            {
+                getLogger().info("ComputerSystem.model: " + computer.getModel());
+            }
+            // Motherboard
+            Baseboard baseboard = computer.getBaseboard();
+            getLogger().info("Motherboard.manufacturer: " + baseboard.getManufacturer());
+            if (!baseboard.getModel().equals("unknown"))
+            {
+                getLogger().info("Motherboard.model: " + baseboard.getModel());
+            }
+            // Disks
+            List<HWDiskStore> disks = hardware.getDiskStores();
+            for (int i=0; i<disks.size(); i++)
+            {
+                String index = (disks.size() == 1) ? "" : "_" + i;
+                getLogger().info("HWDiskStore.name" + index + ": " + disks.get(i).getName());
+                if (!disks.get(i).getModel().equals("unknown"))
+                {
+                    getLogger().info("HWDiskStore.model" + index + ": " + disks.get(i).getModel());
+                }
+                getLogger().info("HWDiskStore.size" + index + ": " + nf.format(disks.get(i).getSize()));
+            }
+            // Networks
+            List<NetworkIF> networks = hardware.getNetworkIFs();
+            for (int i=0; i<networks.size(); i++)
+            {
+                String index = (networks.size() == 1) ? "" : "_" + i;
+                getLogger().info("NetworkIF.name" + index + ": " + networks.get(i).getName());
+                if (!networks.get(i).getDisplayName().equals(networks.get(i).getName()))
+                {
+                    getLogger().info("NetworkIF.displayname" + index + ": " + networks.get(i).getDisplayName());
+                }
+                if (networks.get(i).getSpeed() > 0)
+                {
+                    getLogger().info("NetworkIF.speed" + index + ": " + nf.format(networks.get(i).getSpeed()));
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        
         OperatingSystemMXBean os = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
-        getLogger().info("OperatingSystemMXBean.totalPhysicalMemorySize: " + os.getTotalPhysicalMemorySize());
+        getLogger().info("OperatingSystemMXBean.totalMemorySize: " + nf.format(os.getTotalMemorySize()));
         getLogger().info("Runtime.availableProcessors: " + Runtime.getRuntime().availableProcessors());
-        getLogger().info("Runtime.freeMemory: " + Runtime.getRuntime().freeMemory());
-        getLogger().info("Runtime.maxMemory: " + Runtime.getRuntime().maxMemory());
-        getLogger().info("Runtime.totalMemory: " + Runtime.getRuntime().totalMemory());
+        getLogger().info("Runtime.freeMemory: " + nf.format(Runtime.getRuntime().freeMemory()));
+        getLogger().info("Runtime.maxMemory: " + nf.format(Runtime.getRuntime().maxMemory()));
+        getLogger().info("Runtime.totalMemory: " + nf.format(Runtime.getRuntime().totalMemory()));
         
         for (MemoryPoolMXBean memoryMXBean : ManagementFactory.getMemoryPoolMXBeans())
         {
